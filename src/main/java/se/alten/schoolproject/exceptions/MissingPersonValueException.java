20d7fa34f5a66b1e0023208c12fe177e05cacaf7
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-07 15:32:54 +0000

    Change the name of the class 'MissingStudentValueException' to 'MissingPersonValueException'

--- src/main/java/se/alten/schoolproject/exceptions/MissingPersonValueException.java
+++ src/main/java/se/alten/schoolproject/exceptions/MissingPersonValueException.java
@@ -1,10 +1,10 @@
 package se.alten.schoolproject.exceptions;
 
-public class MissingStudentValueException extends Exception {
+public class MissingPersonValueException extends Exception {
 
     private static final long serialVersionUID = -140110677808863339L;
 
-    public MissingStudentValueException() {
+    public MissingPersonValueException() {
         super("At least one of the required values are missing.");
     }
 
