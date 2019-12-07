f5da41760278602f6b663158b78b66bcb1323c8a
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-07 16:37:45 +0000

    Correct an error in the exception 'TitleNotFoundException'

--- src/main/java/se/alten/schoolproject/exceptions/TitleNotFoundException.java
+++ src/main/java/se/alten/schoolproject/exceptions/TitleNotFoundException.java
@@ -4,8 +4,8 @@ public class TitleNotFoundException extends Exception {
 
     private static final long serialVersionUID = 7023827368623382759L;
 
-    public TitleNotFoundException(String email) {
-        super("A subject with title '" + email + "' could not be found in the database.");
+    public TitleNotFoundException(String title) {
+        super("A subject with title '" + title + "' could not be found in the database.");
     }
 
 }
